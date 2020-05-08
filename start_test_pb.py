0a4062649cc66810dd62222047811bb8386b4fbe
mingxi1997 <49010808+mingxi1997@users.noreply.github.com>
2020-05-08 17:55:36 +0800

    Update start_test_pb.py

--- start_test_pb.py
+++ start_test_pb.py
@@ -6,12 +6,13 @@ import time
 import subprocess
 import  os
 import re
+import time
 table=pd.read_csv('table.csv')
 
 
 def start_test_pb(table,seq,batch):
-  run_metadata = tf.RunMetadata()
   tf.reset_default_graph()
+  run_metadata = tf.RunMetadata()
   input_pb=table.iloc[seq].tasks
 
   input_set=["import/"+input_node[0]+":0" for input_node in eval(table.iloc[seq].input_nodes)]
@@ -20,8 +21,8 @@ def start_test_pb(table,seq,batch):
   output_node=table.iloc[seq].output_node
 
 
-
-  with tf.Session() as sess:  
+  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.99)
+  with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:  
 
     
     with tf.gfile.GFile(input_pb,'rb') as f:  
@@ -54,8 +55,8 @@ def start_test_pb(table,seq,batch):
         sess.run(output_node,feed_dict=feed_dict) 
         
         if step==250 :
-            sess.run(output_node,feed_dict=feed_dict, options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),run_metadata=run_metadata)
-            trace = timeline.Timeline(step_stats=run_metadata.step_stats)
+            sess.run(output_node,feed_dict=feed_dict, options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),run_metadata=tf.RunMetadata())
+            trace = timeline.Timeline(step_stats=tf.RunMetadata().step_stats)
             with open(input_pb[:-3]+'.json' , 'w') as trace_file:
                trace_file.write(trace.generate_chrome_trace_format())
         if step==249:
@@ -66,20 +67,21 @@ def start_test_pb(table,seq,batch):
 
 batch_sizes=table['batch_size']
 for i in range(len(table)):
-      if table.tasks[i].split('.')[-1]=='pb':
-          table.loc[i,'step_time']=start_test_pb(table,i,batch_sizes[i])
+      if table.tasks[i].split('.')[-1]=='pb' :
+#       if 'FP16.pb'in table.tasks[i]:
+#
+#         table.loc[i,'step_time']=start_test_pb(table,i,batch_sizes[i])
+
+         result=start_test_pb(table,i,batch_sizes[i])
+         print('*'*30)
+         print(table.tasks[i])
+         print(result)
+    
+
 #      if table.tasks[i].split('.')[1]=='uff':
 #          table.loc[i,'step_time']=start_test_uff(table,i,[1])
 #      if table.tasks[i].split('.')[1]=='onnx':
 #          table.loc[i,'step_time']=start_test_onnx(table,i,batch_sizes[i])
 
 table.to_csv('table.csv')
-
-
-
-
-
-
-
-
 
