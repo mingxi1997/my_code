55502c67bdb2e241f90622bd9e95e573a310da25
mingxi1997 <49010808+mingxi1997@users.noreply.github.com>
2020-06-24 02:34:12 +0800

    Update track.py

--- track.py
+++ track.py
@@ -1,143 +1,88 @@
-
 import cv2
+import sys
  
-class MessageItem(object):
-    # 用于封装信息的类,包含图片和其他信息
-    def __init__(self,frame,message):
-        self._frame = frame
-        self._message = message
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+if __name__ == '__main__' :
  
-    def getFrame(self):
-        # 图片信息
-        return self._frame
+    # Set up tracker.
+    # Instead of MIL, you can also use
  
-    def getMessage(self):
-        #文字信息,json格式
-        return self._message
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+    tracker_type = tracker_types[2]
  
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
  
-class Tracker(object):
-    '''
-    追踪者模块,用于追踪指定目标
-    '''
+    # Read video
+    video = cv2.VideoCapture(0)
  
-    def __init__(self, tracker_type="BOOSTING", draw_coord=True):
-        '''
-        初始化追踪器种类
-        '''
-        # 获得opencv版本
-        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
-        self.tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-        self.tracker_type = tracker_type
-        self.isWorking = False
-        self.draw_coord = draw_coord
-        # 构造追踪器
-        if int(minor_ver) < 3:
-            self.tracker = cv2.Tracker_create(tracker_type)
-        else:
-            if tracker_type == 'BOOSTING':
-                self.tracker = cv2.TrackerBoosting_create()
-            if tracker_type == 'MIL':
-                self.tracker = cv2.TrackerMIL_create()
-            if tracker_type == 'KCF':
-                self.tracker = cv2.TrackerKCF_create()
-            if tracker_type == 'TLD':
-                self.tracker = cv2.TrackerTLD_create()
-            if tracker_type == 'MEDIANFLOW':
-                self.tracker = cv2.TrackerMedianFlow_create()
-            if tracker_type == 'GOTURN':
-                self.tracker = cv2.TrackerGOTURN_create()
+    # Exit if video not opened.
+    if not video.isOpened():
+        print ("Could not open video")
+        sys.exit()
  
-    def initWorking(self, frame, box):
-        '''
-        追踪器工作初始化
-        frame:初始化追踪画面
-        box:追踪的区域
-        '''
-        if not self.tracker:
-            raise Exception("追踪器未初始化")
-        status = self.tracker.init(frame, box)
-        if not status:
-            raise Exception("追踪器工作初始化失败")
-        self.coord = box
-        self.isWorking = True
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print ('Cannot read video file')
+        sys.exit()
+     
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
  
-    def track(self, frame):
-        '''
-        开启追踪
-        '''
-        message = None
-        if self.isWorking:
-            status, self.coord = self.tracker.update(frame)
-            if status:
-                message = {"coord": [((int(self.coord[0]), int(self.coord[1])),
-                                      (int(self.coord[0] + self.coord[2]), int(self.coord[1] + self.coord[3])))]}
-                if self.draw_coord:
-                    p1 = (int(self.coord[0]), int(self.coord[1]))
-                    p2 = (int(self.coord[0] + self.coord[2]), int(self.coord[1] + self.coord[3]))
-                    cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-                    message['msg'] = "is tracking"
-        return MessageItem(frame, message)
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
  
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
  
-if __name__ == '__main__':
- 
-    # 初始化视频捕获设备
-    gVideoDevice = cv2.VideoCapture(0)
-    gCapStatus, gFrame = gVideoDevice.read()
- 
-    # 选择 框选帧
-    print("按 n 选择下一帧，按 y 选取当前帧")
     while True:
-        if (gCapStatus == False):
-            print("捕获帧失败")
-            quit()
- 
-        _key = cv2.waitKey(0) & 0xFF
-        if(_key == ord('n')):
-            gCapStatus,gFrame = gVideoDevice.read()
-        if(_key == ord('y')):
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
             break
+         
+        # Start timer
+        timer = cv2.getTickCount()
  
-        cv2.imshow("pick frame",gFrame)
+        # Update tracker
+        ok, bbox = tracker.update(frame)
  
-    # 框选感兴趣区域region of interest
-    cv2.destroyWindow("pick frame")
-    gROI = cv2.selectROI("ROI frame",gFrame,False)
-    if (not gROI):
-        print("空框选，退出")
-        quit()
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
  
-    # 初始化追踪器
-    gTracker = Tracker(tracker_type="KCF")
-    gTracker.initWorking(gFrame,gROI)
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
  
-    # 循环帧读取，开始跟踪
-    while True:
-        gCapStatus, gFrame = gVideoDevice.read()
-        if(gCapStatus):
-            # 展示跟踪图片
-            _item = gTracker.track(gFrame)
-            cv2.imshow("track result",_item.getFrame())
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+     
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
  
-            if _item.getMessage():
-                # 打印跟踪数据
-                print(_item.getMessage())
-            else:
-                # 丢失，重新用初始ROI初始
-                print("丢失，重新使用初始ROI开始")
-                gTracker = Tracker(tracker_type="KCF")
-                gTracker.initWorking(gFrame, gROI)
+        # Display result
+        cv2.imshow("Tracking", frame)
  
-            _key = cv2.waitKey(1) & 0xFF
-            if (_key == ord('q')) | (_key == 27):
-                break
-            if (_key == ord('r')) :
-                # 用户请求用初始ROI
-                print("用户请求用初始ROI")
-                gTracker = Tracker(tracker_type="KCF")
-                gTracker.initWorking(gFrame, gROI)
- 
-        else:
-            print("捕获帧失败")
-            quit()
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27 : break
